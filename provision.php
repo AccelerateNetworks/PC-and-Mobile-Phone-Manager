0f64d20322f1ab20dbc001f7dc79c51edef5e20b
Finn <finn@callpipe.com>
2016-01-31 16:13:22 -0800

    SPELL THING RIGHT

--- provision.php
+++ provision.php
@@ -4,7 +4,7 @@ require_once "resources/require.php";
 require_once __DIR__."/resources/utils.php";
 
 if(isset($_GET['secret'])) {
-  $token = do_sql($db, "SELECT * FROM better_provisioning_tokens WHERE secert = :secret", array(":secret" => $_GET['secret']));
+  $token = do_sql($db, "SELECT * FROM better_provisioning_tokens WHERE secret = :secret", array(":secret" => $_GET['secret']));
   if(count($token) > 0) {
     $token = $token[0];
     $extension = do_sql($db, "SELECT * FROM v_extensions WHERE extension_uuid = :extension_uuid", array(':extension_uuid' => $token['extension_uuid']));
