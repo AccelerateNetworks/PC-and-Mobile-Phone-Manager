1541cd2f5e9f7e42a69413b837cd78e07f37bc5b
Finn <finn@callpipe.com>
2016-01-31 16:34:14 -0800

    Non-relative path to the template

--- provisioning/linphone/main.php
+++ provisioning/linphone/main.php
@@ -12,5 +12,5 @@ if(!isset($_GET['noredirect'])) {
   require_once "resources/templates/engine/smarty/Smarty.class.php";
   $smarty = new Smarty();
   $smarty->assign("extension", $extension);
-  $smarty->fetch("linphone-config.xml");
+  $smarty->fetch(__DIR__."/linphone-config.xml");
 }
