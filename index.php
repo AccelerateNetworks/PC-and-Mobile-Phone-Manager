8d93c713b24b8769122c2fbb85e12fe9bef0c22c
Finn <finn@callpipe.com>
2016-02-06 22:53:33 -0800

    send link fix

--- index.php
+++ index.php
@@ -44,11 +44,11 @@ require_once __DIR__."/resources/utils.php";
 				</thead>
 			<?php
 			$rowclass = "row_style0";
-			foreach(do_sql($db, "SELECT token_uuid, type FROM better_provisioning_tokens") as $token) {
+			foreach(do_sql($db, "SELECT token_uuid, type, secret FROM better_provisioning_tokens") as $token) {
 				echo "<tr href=\"edit.php?token_uuid=".$token['token_uuid']."\">";
 				echo "<td class=\"$rowclass\">(we dont have this yet)</td>";
 				echo "<td class=\"$rowclass\">".$token['type']."</td>";
-				echo "<td class=\"$rowclass\">[<a href=\"send.php?token_uuid=".$row['token_uuid']."\">Send</a>]</td>";
+				echo "<td class=\"$rowclass\">[<a href=\"provision.php?secret=".$row['secret']."\" onclick=\"prompt(this.href); return false;>Send</a>]</td>";
 				echo "</tr>";
 				if($rowclass == "row_style0") {
 					$rowclass = "row_style1";
