97d8fed460b715b80f54b0bf29c1bb0b1c64bdd8
Finn <finn@callpipe.com>
2016-02-07 17:49:58 -0800

    Only consider the UUID if the input is a valid UUID

--- extension-suggest.php
+++ extension-suggest.php
@@ -10,14 +10,18 @@ require_once __DIR__."/resources/utils.php";
 if(isset($_GET['q'])) {
   $extension = $_GET['q'];
   $domain = "";
+  $uuid = "";
   if(strpos($_GET['q'], '@')) {
     list($extension,$domain) = explode("@", $_GET['q'], 2);
   }
+  if(count(preg_match("/[\da-f]{8}-?([\da-f]{4}-?){3}[\da-f]{12}/i", $_GET['q']) == 1)) {
+    $uuid = $_GET['q'];
+  }
 
-  $results = do_sql($db, "SELECT v_extensions.extension_uuid,v_extensions.extension,v_domains.domain_name FROM v_extensions FULL JOIN v_domains ON v_extensions.domain_uuid = v_domains.domain_uuid WHERE (v_extensions.extension LIKE :extension AND v_domains.domain_name LIKE :domain) OR (v_extensions.extension_uuid = :query) LIMIT 30;", array(
+  $results = do_sql($db, "SELECT v_extensions.extension_uuid,v_extensions.extension,v_domains.domain_name FROM v_extensions FULL JOIN v_domains ON v_extensions.domain_uuid = v_domains.domain_uuid WHERE (v_extensions.extension LIKE :extension AND v_domains.domain_name LIKE :domain) OR (v_extensions.extension_uuid = :uuid) LIMIT 30;", array(
     ':extension' => "%".$extension."%",
     ':domain' => "%".$domain."%",
-    ':query' => $_GET['q']
+    ':uuid' => $_GET['q']
   ));
   header('Conte-Type: application/json');
   echo json_encode($results);
