e5b9e5273055c3db4312490f6fadc5d0182d286e
Finn <finn@callpipe.com>
2016-02-07 16:32:48 -0800

    Move the % to the arguments

--- extension-suggest.php
+++ extension-suggest.php
@@ -13,10 +13,11 @@ if(isset($_GET['q'])) {
   if(strpos($_GET['q'], '@')) {
     list($extension,$domain) = explode("@", $_GET['q'], 2);
   }
-  $results = do_sql($db, "SELECT v_extensions.extension_uuid,v_extensions.extension,v_extensions.number_alias,v_extensions.accountcode,v_extensions.enabled,v_extensions.description,v_domains.domain_name,v_domains.domain_enabled FROM v_extensions FULL JOIN v_domains ON v_extensions.domain_uuid = v_domains.domain_uuid WHERE (v_extensions.extension LIKE %:extension% OR v_extensions.number_alias LIKE %:number_alias%) AND v_domains.domain_name LIKE %:domain%;", array(
-    ':extension' => $extension,
-    ':number_alias' => $extension,
-    ':domain' => $domain
+
+  $results = do_sql($db, "SELECT v_extensions.extension_uuid,v_extensions.extension,v_extensions.number_alias,v_extensions.accountcode,v_extensions.enabled,v_extensions.description,v_domains.domain_name,v_domains.domain_enabled FROM v_extensions FULL JOIN v_domains ON v_extensions.domain_uuid = v_domains.domain_uuid WHERE (v_extensions.extension LIKE :extension OR v_extensions.number_alias LIKE :number_alias) AND v_domains.domain_name LIKE :domain;", array(
+    ':extension' => "%".$extension."%",
+    ':number_alias' => "%".$extension."%",
+    ':domain' => "%".$domain."%"
   ));
   echo json_encode($results);
 } else {
