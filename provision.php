8b198f5d5c2b24a794343bf8faa0497b55078d4d
Finn <finn@callpipe.com>
2016-03-19 20:57:55 -0700

    use array_key_exists

--- provision.php
+++ provision.php
@@ -18,7 +18,7 @@ if(isset($_GET['secret'])) {
   preg_match("/\/cfg(?<mac>[a-z0-9]{12})\.xml/", $_SERVER['QUERY_STRING'], $mac_regex);
   if(isset($mac_regex['mac'])) {
     $prefix = substr($mac_regex['mac'], 0, 6);
-    if(isset($oui_prefix[$prefix])) {
+    if(array_key_exists($prefix, $oui_prefix)) {
       $type = $oui_prefix[$prefix];
       require(__DIR__."/provisioning/".$type."/main.php");
       do_initial_provision($mac_regex['mac']);
